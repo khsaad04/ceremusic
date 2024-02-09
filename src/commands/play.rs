dd26a98c7c7f24aed68073f8f895648d6f96f441
khsaad04 <khsaad0404@gmail.com>
2024-02-09 16:11:39 +0600

    play cmd

--- src/commands/play.rs
+++ src/commands/play.rs
@@ -9,8 +9,27 @@ pub async fn play(
     #[description = "The song you want to play"]
     query: String,
 ) -> Result<(), Error> {
+    let channel_id = ctx
+        .guild()
+        .unwrap()
+        .voice_states
+        .get(&ctx.author().id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    let _vc = match channel_id {
+        Some(channel) => channel,
+        None => {
+            ctx.say("Not in a vc").await?;
+            return Ok(());
+        }
+    };
+    let manager = songbird::get(ctx.serenity_context()).await.unwrap();
+    let handle_lock = manager
+        .join(ctx.guild_id().unwrap(), channel_id.unwrap())
+        .await?;
+
+    let mut handler = handle_lock.lock().await;
     let url = query;
-    let guild_id = ctx.guild_id().unwrap();
 
     let http_client = {
         let data = ctx.serenity_context().data.read().await;
@@ -18,20 +37,9 @@ pub async fn play(
             .cloned()
             .expect("Guaranteed to exist in the typemap.")
     };
+    let src = YoutubeDl::new(http_client, url);
 
-    let manager = songbird::get(ctx.serenity_context())
-        .await
-        .expect("Songbird Voice client placed in at initialization.")
-        .clone();
+    let _ = handler.play_input(src.clone().into());
 
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let mut handler = handler_lock.lock().await;
-        let src = YoutubeDl::new(http_client, url);
-
-        let _ = handler.play_input(src.clone().into());
-        ctx.say("Playing song").await?;
-    } else {
-        ctx.say("Not in a voice channel").await?;
-    }
     Ok(())
 }
